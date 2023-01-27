3cf99d52bee4592eb67ef0ce42cabcd5c4045f3e
kylesava <77376150+kylesava@users.noreply.github.com>
2023-01-27 13:10:10 -0800

    finished visual template for grading and display

--- start.py
+++ start.py
@@ -1,15 +1,34 @@
-import pygame
+import pygame as p
   
-pygame.init()  
-programIcon = pygame.image.load('assets/icon.png')
+p.init()  
+programIcon = p.image.load('assets/icon.png')
 
-pygame.display.set_caption('QuickGrader')  
-pygame.display.set_icon(programIcon)
-screen = pygame.display.set_mode((600,600))  
+p.display.set_caption('QuickGrader')  
+p.display.set_icon(programIcon)
+screenX = 1500
+screenY = 900
+screen = p.display.set_mode((screenX,screenY))  
+
+
+############################### Graphics Functions ###############################
+def template():
+    screen.fill(0x000000)
+    mediaBg = p.image.load('assets/mediaBackground.png')
+    mediaBg = p.transform.scale(mediaBg, (screenY,screenY))
+    screen.blit(mediaBg,(0,0))
+    gradeBg = p.image.load('assets/gradingBackground.png')
+    gradeBg = p.transform.scale(gradeBg, (int(2*screenY/3),screenY))
+    screen.blit(gradeBg,(screenY+1,0))
+    #p.draw.rect(screen,0xaaaaaa,(0,0,int(screenX*2/3),screenY))
+##################################################################################
+
 done = False  
-  
+template()
 while not done:  
-    for event in pygame.event.get():  
-        if event.type == pygame.QUIT:  
+    for event in p.event.get():  
+        if event.type == p.QUIT:  
             done = True  
-    pygame.display.flip()  
+        elif event.type == p.MOUSEBUTTONDOWN:
+            position=event.pos
+            p.draw.rect(screen,(255,255,255),(position[0],position[1],20,20))
+    p.display.flip()  
