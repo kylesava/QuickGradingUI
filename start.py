0b15d1c200cd9647e84ba35b417b245082374202
kylesava <77376150+kylesava@users.noreply.github.com>
2023-01-27 22:11:39 -0800

    implemented class structures and set up initial prototype cases, reworked some UI

--- start.py
+++ start.py
@@ -1,4 +1,5 @@
 import pygame as p
+from studentobj import Student,File
   
 p.init()  
 programIcon = p.image.load('assets/icon.png')
@@ -11,24 +12,45 @@ screen = p.display.set_mode((screenX,screenY))
 
 
 ############################### Graphics Functions ###############################
-def template():
-    screen.fill(0x000000)
-    mediaBg = p.image.load('assets/mediaBackground.png')
-    mediaBg = p.transform.scale(mediaBg, (screenY,screenY))
-    screen.blit(mediaBg,(0,0))
-    gradeBg = p.image.load('assets/gradingBackground.png')
-    gradeBg = p.transform.scale(gradeBg, (int(2*screenY/3),screenY))
-    screen.blit(gradeBg,(screenY+1,0))
+def template(mode=6):
+    if mode%2==0:
+        mediaBg = p.image.load('assets/mediaBackground.png')
+        mediaBg = p.transform.scale(mediaBg, (screenY,screenY))
+        screen.blit(mediaBg,(0,0))
+    if mode%3==0:
+        gradeBg = p.image.load('assets/gradingBackground.png')
+        gradeBg = p.transform.scale(gradeBg, (int(2*screenY/3),screenY))
+        screen.blit(gradeBg,(screenY+1,0))
     #p.draw.rect(screen,0xaaaaaa,(0,0,int(screenX*2/3),screenY))
-##################################################################################
 
+def displayMedia(student, fileno):
+    template(2)
+    media = student.files[fileno].media
+    screen.blit(media,(int(screenY/2-(media.get_width()/2)), int(screenY/2-(media.get_height()/2))))
+
+def displayFiles(student, fileno):
+    template(3)
+    for i in range(len(student.files)):
+        introtext = p.font.Font('Fonts\KOMIKAX_.ttf', 15)
+        if i==fileno:
+            print("fart")
+            
+##################################################################################
+########Test students
+student1 = Student(4002,"Kyle","Sava")
+fileimg = p.image.load('assets/runmean.png')
+file1 = File(fileimg,"Runmean","fig")
+student1.addfile(file1)
+print(student1)
+########
 done = False  
 template()
+displayMedia(student1,0)
 while not done:  
     for event in p.event.get():  
         if event.type == p.QUIT:  
             done = True  
-        elif event.type == p.MOUSEBUTTONDOWN:
+        elif event.type == p.MOUSEMOTION:
             position=event.pos
             p.draw.rect(screen,(255,255,255),(position[0],position[1],20,20))
     p.display.flip()  
---
+++ studentobj.py
@@ -0,0 +1,31 @@
+import pygame as p
+screenX = 1500
+screenY = 900
+class File:
+    def __init__(self, media, name, type, grade=0, reviewed=False):
+        if media.get_width() >= media.get_height():
+            self.media = p.transform.scale(media, (int(8*screenY/9),int(media.get_height()*((8*screenY/9)/media.get_width()))))   
+        else:
+            self.media = p.transform.scale(media, (int(media.get_width()*((8*screenY/9)/media.get_height())),int(8*screenY/9)))
+        self.name = name
+        self.type = type
+        self.grade = grade
+        self.reviewed = reviewed
+    
+    def changegrade(self,grade):
+        self.grade=grade
+        self.reviewed=True
+
+
+class Student:
+    def __init__(self, number=0, fname="", lname=""):
+        self.fname = fname
+        self.lname = lname
+        self.number = number
+        self.files=[]
+    def addfile(self,file):
+        self.files.append(file)
+    def getfiles(self):
+        return self.files
+    
+ 
